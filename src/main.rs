0b6042c8032bb28ec411997a678eaea98ec87eb6
RealPhonki <phonkibusiness@gmail.com>
2026-08-18 12:29:32 -0400

    Created random move bot

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,50 @@ mod move_list;
 mod debug;
 
 // third party
-use cozy_chess::Board;
+use cozy_chess::{Board, Move};
+use rand::RngExt;
 
 // project
 use move_list::MoveList;
 use debug::print_board;
 
+// std
+use std::io;
+
 fn main() {
     // Start position
-    let board = Board::default();
+    let mut board = Board::default();
     print_board(&board);
 
-    // get legal moves
-    let mut move_list = MoveList::new();
-    board.generate_moves(|moves| {
-        for mv in moves {
-            move_list.push(mv);
-        }
-        return false
-    });
+    for _ in 0..100 {
+        // Todo: Refuse illegal moves
+        // Todo: Exit upon game completion
+        // Todo: Isolate bot code to separate file
 
-    for index in 0..(move_list.count()) {
-        println!("{}", move_list[index].unwrap());
+        // get user input
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let player_move = input.trim_end().parse::<Move>().unwrap();
+        board.play(player_move);
+        
+        // get legal moves
+        let mut move_list = MoveList::new();
+        board.generate_moves(|moves| {
+            for mv in moves {
+                move_list.push(mv);
+            }
+            return false
+        });
+
+        // play random move
+        let move_index = rand::rng().random_range(0..move_list.count());
+        board.play(move_list[move_index].unwrap());
+        print_board(&board);
     }
 }
+
+/* print moves
+for index in 0..(move_list.count()) {
+    println!("{}", move_list[index].unwrap());
+}
+*/
